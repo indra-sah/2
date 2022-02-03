6709c209cf3e59f88006d6f897f2fbe2982f4758
indra-sah <83999954+indra-sah@users.noreply.github.com>
2022-02-03 22:46:22 +0545

    Initial commit

---
+++ src/college/Main.java
@@ -0,0 +1,27 @@
+package college;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//Comparison Operators
+        //a == b
+        //a != b
+        //a < b
+        //a > b
+        //a <= b
+        //a >= b
+        
+
+
+
+
+
+
+
+
+
+    }
+}
